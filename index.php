c862da9daf7cad3f01f5fad666d65f286ed3e65b
CC-Gold-PGW <56265309+CC-Gold-PGW@users.noreply.github.com>
2021-03-23 02:42:52 +0000

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,6 @@ mysqli_query($con, "SELECT `Counter`.`Yea` FROM `Counter`= counter + 1");
 //Retrieves the current count
 $count = mysqli_query($con, "SELECT `Counter`.`Yea` FROM `Counter`;");
 //Displays the count on your site
-echo "$count"; 
 
 $row = mysqli_fetch_array($count, MYSQLI_NUM);
 
