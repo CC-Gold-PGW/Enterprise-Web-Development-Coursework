096739d69af72b019baddb34d9f572f960f8c2f9
CC-Gold-PGW <56265309+CC-Gold-PGW@users.noreply.github.com>
2021-03-23 17:26:55 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost","my_user","my_password","my_db");
+$con=mysqli_connect("localhost","root","Gx2U55b8aMkeXpF5","Counter");
 
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
